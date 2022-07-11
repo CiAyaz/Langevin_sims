5a46830f94f05b3d76d68dc6b7fe3bd626fe37ca
Cihan Ayaz <cihanayaz@physik.fu-berlin.de>
2022-07-11 08:19:46 +0200

    setup.py updated

--- v01/setup.py
+++ v01/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     # cihan langevin
-    setup(name='Langevin_sims',
+    setup(name='Lan_sims',
           packages=find_packages(),
           version="1.0",
           license='MIT',
